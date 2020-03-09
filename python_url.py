6ab7db739cb06a4fd7ab707e7a20839047aaf871
hanonymus90 <52471384+hanonymus90@users.noreply.github.com>
2020-03-09 22:01:30 +0100

    Delete python_url.py

--- python_url.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-url = input("Type wich website you want to open (example wise youtube.com): ")
-webbrowser.open_new_tab("https://www."+ url)
-print("Thanks for using me!")
-exit()
