b2154996f7735161bc9451f422b0ff075e4f4b6f
hanonymus90 <52471384+hanonymus90@users.noreply.github.com>
2020-03-09 22:02:21 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+import webbrowser
+url = input("Type wich website you want to open (example wise youtube.com): ")
+webbrowser.open_new_tab("https://www."+ url)
+print("Thanks for using me!")
+exit()
